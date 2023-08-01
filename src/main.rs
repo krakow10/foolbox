1b5a1985b767d82ca6f7e036a1ab0eb5d7433c6b
Quaternions <krakow20@gmail.com>
2023-07-31 21:36:03 -0700

    efficient top64

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-	let result;
-	{
-		let numba=2;
-
-		result=&numba;
-		//numba dies
-	}
-	println!("{:?}",result);
+	let a:u64 = 0x00000000_90000000;
+	let b:u64 = 0x00000010_00000000;
+	let c:u64 = (((a as u128)*(b as u128))>>64) as u64;
+	println!("{:?}",c);
 }
