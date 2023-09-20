8fda9220f43fdb52e2ae050ac318cbf04f7fae9b
Quaternions <krakow20@gmail.com>
2023-09-19 23:20:32 -0700

    ranges are kinda cringe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-	let a:u64 = 0x00000000_90000000;
-	let b:u64 = 0x00000010_00000000;
-	let c:u64 = (((a as u128)*(b as u128))>>64) as u64;
-	println!("{:?}",c);
+	let r=1..=5;
+	println!("start:{} end:{}",r.start(),r.end());
 }
