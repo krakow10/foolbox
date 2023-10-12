187524a48f6a2756f84c2916146d17aafc43c075
Quaternions <krakow20@gmail.com>
2023-10-12 04:47:19 -0700

    sanity test u32/2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
 fn main() {
-	for radicand in 0..u32::MAX{
-		//interpret bits as float
-		let f_sqrt:u32=unsafe{(radicand as f32).sqrt().to_int_unchecked()};
-		if f_sqrt.saturating_add(1)*f_sqrt.saturating_add(1)<radicand||radicand<f_sqrt.saturating_sub(1)*f_sqrt.saturating_sub(1){
-			println!("rad:{} diff:{}",radicand,radicand as i64-(f_sqrt*f_sqrt) as i64);
-		}
-	}
+	println!("{}",(u32::MAX/2) as i32);
 }
