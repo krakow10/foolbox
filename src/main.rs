a87c0067720d67b237df09cbef670be21c1a8580
Quaternions <krakow20@gmail.com>
2023-10-11 23:55:03 -0700

    int neg test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-	for _ in 0..1000 {
-		let start=std::time::Instant::now();
-		std::thread::spawn(move || {
-			println!("{}",start.elapsed().as_nanos());
-		});
-		//wait for the result to be printed to console
-		std::thread::sleep(std::time::Duration::from_millis(100));
-	}
+	let mut n:i32=i32::MIN;
+	println!("{:?}",n);
+	n=n.wrapping_neg();
+	println!("{:?}",n);
 }
