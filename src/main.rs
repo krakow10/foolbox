5038e86290d98340246af3b26f1555af4dfb0cb1
Quaternions <krakow20@gmail.com>
2023-10-12 00:06:30 -0700

    test f sqrt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-	let mut n:i32=i32::MIN;
-	println!("{:?}",n);
-	n=n.wrapping_neg();
-	println!("{:?}",n);
+	for radicand in 0..u32::MAX{
+		//interpret bits as float
+		let f_sqrt:u32=unsafe{(radicand as f32).sqrt().to_int_unchecked()};
+		if f_sqrt.saturating_add(1)*f_sqrt.saturating_add(1)<radicand||radicand<f_sqrt.saturating_sub(1)*f_sqrt.saturating_sub(1){
+			println!("rad:{} diff:{}",radicand,radicand as i64-(f_sqrt*f_sqrt) as i64);
+		}
+	}
 }
