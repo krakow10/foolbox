5829f220671962f06ca7c57a8bfa15c9ed52a9a2
Quaternions <krakow20@gmail.com>
2023-11-15 01:25:13 -0800

    signum

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
-const V: [[i32; 3]; 8]=[
-	[-1,-1, 1],
-	[ 1,-1, 1],
-	[ 1, 1, 1],
-	[-1, 1, 1],
-	[-1, 1,-1],
-	[ 1, 1,-1],
-	[ 1,-1,-1],
-	[-1,-1,-1],
-];
+fn s(s:usize)->isize{
+	(s as isize).signum()
+}
 fn main() {
-	let mut e=Vec::new();
-	for i in 0..V.len(){
-		for j in i..V.len(){
-			let v0=V[i];
-			let v1=V[j];
-			let mut c=0;
-			if v0[0]==v1[0]{
-				c+=1;
-			}
-			if v0[1]==v1[1]{
-				c+=1;
-			}
-			if v0[2]==v1[2]{
-				c+=1;
-			}
-			if c==2{
-				e.push([i,j]);
-			}
-		}
-	}
-	println!("{:?}",e);
+	let a=1isize;
+	let v=vec![1];
+	println!("{}",v[a]);
 }
