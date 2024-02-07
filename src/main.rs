c6ad20b449ecf4fe016bb58ee91a3e5de619c7a0
Quaternions <krakow20@gmail.com>
2024-02-06 21:00:26 -0800

    is vec newtype zero cost

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
-fn s(s:usize)->isize{
-	(s as isize).signum()
+type Mesh=i32;
+struct MeshId(u32);
+struct MeshVec(Vec<Mesh>);
+
+impl std::ops::Index<MeshId> for MeshVec{
+	type Output=Mesh;
+	fn index(&self, index: MeshId) -> &Self::Output {
+		&self.0[index.0 as usize]
+	}
 }
+impl std::ops::Deref for MeshVec{
+	type Target = Vec<Mesh>;
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
+}
+
 fn main() {
-	let a=1isize;
-	let v=vec![1];
-	println!("{}",v[a]);
+	let ilist=vec![1,2,3];
+	//a
+	let m=MeshVec(ilist.clone());
+	let a=m[MeshId(1)];
+	println!("a={}",a);
+	//b
+	let b=ilist[1];
+	println!("b={}",b);
+
+	//iter
+	for v in m.iter(){
+		println!("v={}",v);
+	}
+	let c=std::borrow::Cow::Borrowed(&ilist);
+	for v in c.iter(){
+		println!("v={}",v);
+	}
 }
