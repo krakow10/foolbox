2ad16cf4b9d5dbba468c798738a987a173f7d28d
Quaternions <krakow20@gmail.com>
2024-03-10 21:55:35 -0700

    prove serialization

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use binrw::BinWrite;
+
+#[binrw::binrw]
+#[brw(little)]
+pub struct Header2{
+	sizeof_header:u16,//size of this struct...
+	sizeof_vertex:u8,
+	sizeof_face:u8,
+}
+
 fn main() {
-	let s="this is a test";
-	for c in s.split(""){
-		println!("{c}");
-	}
+	let h=Header2{
+		sizeof_header:12,
+		sizeof_vertex:40,
+		sizeof_face:12,
+	};
+	let mut buf=Vec::new();
+	let mut c=std::io::Cursor::new(&mut buf);
+	h.write_le(&mut c).unwrap();
+	println!("{:?}",c);
+	println!("hi");
 }
