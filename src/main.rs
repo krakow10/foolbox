c98830469071534b60e1410fd2dc89528189d798
Quaternions <krakow20@gmail.com>
2024-03-23 17:44:32 -0700

    swap

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,6 @@
-struct Circle{
-	radius:f32,
-}
-impl Circle{
-	fn new(radius:f32)->Self{
-		Self{
-			radius
-		}
-	}
-	fn get_area(&self)->f32{
-		std::f32::consts::PI*self.radius*self.radius
-	}
-	fn get_circumference(&self)->f32{
-		std::f32::consts::TAU*self.radius
-	}
-}
 fn main(){
-	let c=Circle::new(6.0);
-	println!("The radius is {}",c.radius);
-	println!("The area is {}",c.get_area());
-	println!("The circumference is {}",c.get_circumference());
+	let mut a=1;
+	let mut b=2;
+	std::mem::swap(&mut a,&mut b);
+	println!("a={} b={}",a,b);
 }
