e8e8c010521e70a958e61b201206da24fc041d79
Quaternions <krakow20@gmail.com>
2024-04-08 00:00:30 -0700

    stuff

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ fn main(){
 	let t2=[2,4,6];
 	let t_final:Vec<u32>=t1
 	.into_iter()
-	.zip(t2.into_iter())
-	.flat_map(|(v0,v1)| [v0,v1].into_iter())
+	.zip(t2)
+	.flat_map(|(v0,v1)| [v0,v1])
 	.collect();
 	println!("{:?}",t_final);
 }
