cd5f9d5ce904330cc5565b35f28542d70eff8536
Quaternions <krakow20@gmail.com>
2024-04-10 02:30:44 -0700

    zz9c post

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-
 fn main(){
-	let mut s=String::from("hello");
-	let b=unsafe{s.as_bytes_mut()};
-  //if(97..123).contains(&b[0]){b[0]-=32;}
-  //if 96<b[0]&&b[0]<123{b[0]-=32}
-  //if let 97..=122=b[0]{b[0]^=32}
-	b[0].make_ascii_uppercase();
-	println!("{}",s);
+	loop{
+		loop{
+			loop{
+				for i in 0..=1000{
+					println!("{i}"); 
+				}
+			}
+		}
+	}
 }
