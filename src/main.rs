980f66cc2e55b0f7f2d826811866d89371096f8c
Quaternions <krakow20@gmail.com>
2024-05-05 01:21:52 -0700

    capitalize

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-fn main(){
-	loop{
-		loop{
-			loop{
-				for i in 0..=1000{
-					println!("{i}"); 
-				}
-			}
-		}
+fn capitalize(s:&str)->String{
+	if s.len()==0{
+		return "".to_owned();
 	}
+	let a=s[0..1].to_uppercase();
+	let b=a+&s[1..];
+	b
+}
+fn main(){
+	println!("{}",capitalize(""));
 }
