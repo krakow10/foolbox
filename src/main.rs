a92067c95e4ff3d24934200140af37ef09985759
Quaternions <krakow20@gmail.com>
2024-05-24 18:10:50 -0700

    find lowest

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main(){
-	let a;
-	let mut b = Box::new(0);// dynamic allocation
-	a = 4;
-	*b = 5;
-	let c = a + *b;
-	println!("{c}"); // prints 9
-	//b is dropped when the scope ends
+	let t = [9,3,7,3,7,32,76,8,3,21];
+	let smallest = t.iter().fold(std::i32::MAX,|s,&v|if v<s{v}else{s});
+	println!("smallest {smallest}");
 }
