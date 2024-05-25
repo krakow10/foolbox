8374745fb7e7094721e319caa5f3e3e4932e3c7a
Quaternions <krakow20@gmail.com>
2024-05-24 19:22:27 -0700

    _at

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 fn main(){
-	let t = [9,3,7,3,7,32,76,8,3,21];
-	let smallest = t.iter().fold(std::i32::MAX,|s,&v|if v<s{v}else{s});
-	println!("smallest {smallest}");
+	//these values have the type &str which is called "string slice"
+	let words = ["cat","bat","rat","mat","that","chair","sat","me","at"];
+	let _at: Vec<&str> = 
+	words
+	//convert the words array into an iterator, consuming it
+	.into_iter()
+	//filter out values according to a function that returns true or false
+	.filter(|&word|{
+		if word.len()!=3{
+			//filter out words not length 3
+			return false;
+		}
+		match &word[1..3]{ //this operation is "slice"
+			"at"=>true,
+			//filter out words that don't have "at" at character positions 1..3 (non-inclusive range)
+			_=>false,
+		}
+	})
+	//collect the iterator into a Vec<&str>
+	.collect();
+	println!("filtered words {_at:?}"); // :? means debug print
 }
