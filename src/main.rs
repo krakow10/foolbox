7a95e7dae4df0685a9dc55b8fe166f522c9f51b7
Quaternions <krakow20@gmail.com>
2024-06-10 06:48:08 -0700

    hashset

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-use std::collections::HashMap;
-
 fn main(){
-	//big ole string
-	let s = "the quick brown fox jumps over the lazy dog";
-	//create a hashmap, h variable is mutable
-	let mut h = HashMap::new();
-	//loop over string characters
-	for c in s.chars(){
-		//handy rust tool "Entry"
-		h.entry(c)
-		//if the entry exists, add one to it
-		.and_modify(|v|*v+=1)
-		//otherwise, insert an entry for c and set it to 1
-		.or_insert(1);
-	}
-	//print out the hashmap via debug print
-	println!("{h:?}");
+	let admins:std::collections::HashSet<u64>=[52250025,2349947797,5741161789,1383928418,147260781].into();
 }
