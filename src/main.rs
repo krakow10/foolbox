907c77c4c450c670cc601defce47fba9f5c14a55
Quaternions <krakow20@gmail.com>
2024-07-24 16:16:54 -0700

    check int cast

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 fn main(){
-	//create a hashmap
-	let mut h = std::collections::HashMap::new();
-
-	//insert a key-value pair
-	h.insert("key1","value1");
-
-	//access the value using the key
-	println!("{}",h["key1"]);// -> value1
+	let n=257u128;
+	println!("{}",n as u8);
 }
