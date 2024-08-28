1bab58208d6423476c03f12a6b29c0c5094ca0c3
Quaternions <krakow20@gmail.com>
2024-08-28 16:01:04 -0700

    explore float gcd

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+fn gcd(mut a:f64,mut b:f64)->f64{
+	while b!=0f64{
+		(a,b)=(b,a.rem_euclid(b));
+	};
+	a
+}
+
 fn main() {
-    let mut lines_list = vec!["line1", "line2", "line3", "line4"];
-    let mut it = lines_list.iter();
-
-	it.next();
-	std::mem::drop(it);
-	lines_list.pop();
-	it=lines_list.iter();
-
-    // iterator still exists and can be used
-    for line in it {
-        println!("{}", line);
-    }
+	let a=0.1298374568127365872345;
+	let b=897231895434.23456234859723495;
+	let c=gcd(a,b);
+	println!("{}",c);
+	println!("{}",a/c);
+	println!("{}",b/c)
 }
