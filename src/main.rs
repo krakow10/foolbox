927bda25c43da94ae5ddd2020dba121b6739d024
Quaternions <krakow20@gmail.com>
2024-08-28 16:01:13 -0700

    does this make sense

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-fn gcd(mut a:f64,mut b:f64)->f64{
-	while b!=0f64{
+const fn gcd(mut a:u128,mut b:u128)->u128{
+	while b!=0{
 		(a,b)=(b,a.rem_euclid(b));
 	};
 	a
 }
 
 fn main() {
-	let a=0.1298374568127365872345;
-	let b=897231895434.23456234859723495;
-	let c=gcd(a,b);
+	const div:u128=10000000000000000000000u128;
+	const a:u128=1298374568127365872345u128;
+	const b:u128=8972318954342345623485972349500000u128;
+	const c:u128=gcd(a,b);
 	println!("{}",c);
 	println!("{}",a/c);
-	println!("{}",b/c)
+	println!("{}",b/c);
+	println!("{}",c as f64/div as f64);
 }
