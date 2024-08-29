43e6551b12c67a70821e59dba48b65ffc28cc2b8
Quaternions <krakow20@gmail.com>
2024-08-29 15:58:37 -0700

    time 512 bit sqrt (15.78µs)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-const fn gcd(mut a:u128,mut b:u128)->u128{
-	while b!=0{
-		(a,b)=(b,a.rem_euclid(b));
-	};
-	a
-}
 
 fn main() {
-	const div:u128=10000000000000000000000u128;
-	const a:u128=1298374568127365872345u128;
-	const b:u128=8972318954342345623485972349500000u128;
-	const c:u128=gcd(a,b);
-	println!("{}",c);
-	println!("{}",a/c);
-	println!("{}",b/c);
-	println!("{}",c as f64/div as f64);
+	let a=fixed_wide::types::I256F256::ONE*2;
+	let d=std::time::Instant::now();
+	let r=a.sqrt();
+	println!("t={:?}",d.elapsed());
+	println!("ans={:?}",r);
 }
