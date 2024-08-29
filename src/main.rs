d8c8c8d0f8425479259afdb21f405bd249af82a0
Quaternions <krakow20@gmail.com>
2024-08-29 16:01:23 -0700

    64

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 fn main() {
-	let a=fixed_wide::types::I256F256::ONE*2;
+	let a=fixed_wide::types::I32F32::ONE*2;
 	let d=std::time::Instant::now();
 	let r=a.sqrt();
 	println!("t={:?}",d.elapsed());
