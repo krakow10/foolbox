c40ae40c611ef282635893d16e8e315fc5fd197c
Quaternions <krakow20@gmail.com>
2024-09-04 15:35:00 -0700

    matrix implementation

--- src/main.rs
+++ src/main.rs
@@ -53,8 +53,79 @@ impl From<[f32;3]> for MagicVector3{
 	}
 }
 
+// Array-backed Matrix with named field access
+
 // Mat<3,Vec<2>>.dot(M)
 // M.x_axis
+// 	self.map(|axis|
+// 		tr.map(|trax|
+// 			axis.dot(trax)
+// 		).to_vector()
+// 	)
+#[derive(Clone,Copy)]
+struct Array2d<const X:usize,const Y:usize>([[f32;Y];X]);
+impl<const X:usize,const Y:usize> Array2d<X,Y>{
+	fn dot<const Z:usize>(self,other:Array2d<Y,Z>)->Array2d<X,Z>{
+		let mut tr=[[0f32;Y];Z];
+		for y in 0..Y{
+			for z in 0..Z{
+				tr[z][y]=other.0[y][z];
+			}
+		}
+		Array2d(self.0.map(|axis|
+			tr.map(|trax|
+				Array(axis).dot(Array(trax))
+			)
+		))
+	}
+}
+
+#[derive(Clone,Copy)]
+struct Matrix3{
+	x_axis:MagicVector3,
+	y_axis:MagicVector3,
+	z_axis:MagicVector3,
+}
+
+#[derive(Clone,Copy)]
+union MagicMatrix3 {
+	array:Array2d<3,3>,
+	named:Matrix3,
+}
+
+impl MagicMatrix3{
+	fn dot(self,other:MagicMatrix3)->MagicMatrix3{
+		MagicMatrix3{
+			array:unsafe{
+				self.array.dot(other.array)
+			}
+		}
+	}
+}
+
+impl std::ops::Deref for MagicMatrix3{
+	type Target=Matrix3;
+	fn deref(&self)->&Self::Target{
+		unsafe{
+			&self.named
+		}
+	}
+}
+impl std::ops::DerefMut for MagicMatrix3{
+	fn deref_mut(&mut self)->&mut Self::Target{
+		unsafe{
+			&mut self.named
+		}
+	}
+}
+
+impl From<[[f32;3];3]> for MagicMatrix3{
+	fn from(value:[[f32;3];3])->Self{
+		Self{
+			array:Array2d(value)
+		}
+	}
+}
 
 fn main(){
 	let mut v=MagicVector3::from([1.0,2.0,3.0]);
@@ -62,4 +133,10 @@ fn main(){
 	v.x*=5.0;//wow
 	println!("dot={dotty}");
 	println!("v.x={}",v.x);//wow!
+
+	let mut m=MagicMatrix3::from([[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]]);
+	let mdotty=m.dot(m);
+	m.x_axis=MagicVector3::from([-1.0,-2.0,-3.0]);
+	println!("mat mul xx={}",mdotty.x_axis.x);
+	println!("m.x_axis.x={}",m.x_axis.x);
 }
