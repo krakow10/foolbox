604bfa77ae8d9d172bf9a8e3a0382c1b032decd2
Quaternions <krakow20@gmail.com>
2024-09-04 14:42:22 -0700

    named field access of array-backed vector

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,49 @@
-const JUMP_TIME:f32=0.715588;//78/109
-fn main() {
-	let (i,f)=(1..200).map(|i|(i,JUMP_TIME*i as f32)).min_by_key(|&(_,f0)|{
-		let fr=f0.fract();
-		(fr.min(1.0-fr)*(1u64<<32) as f32) as u32
-	}).unwrap();
-	println!("min index: {i}\nmin value:{f}");
+// Array-backed vector with named field access
+
+#[derive(Clone,Copy)]
+struct Array<const N:usize>([f32;N]);
+impl<const N:usize> Array<N>{
+	fn dot(self,other:Self)->f32{
+		self.0.into_iter().zip(other.0).map(|(a,b)|a*b).sum()
+	}
+}
+
+#[derive(Clone,Copy)]
+struct Vector3{
+	x:f32,
+	y:f32,
+	z:f32,
+}
+
+#[derive(Clone,Copy)]
+union MagicVector3 {
+	array:Array<3>,
+	named:Vector3,
+}
+
+impl MagicVector3{
+	fn dot(self,other:MagicVector3)->f32{
+		unsafe{
+			self.array.dot(other.array)
+		}
+	}
+}
+
+impl std::ops::Deref for MagicVector3{
+	type Target=Vector3;
+	fn deref(&self)->&Self::Target{
+		unsafe{
+			&self.named
+		}
+	}
+}
+
+// Mat<3,Vec<2>>.dot(M)
+// M.x_axis
+
+fn main(){
+	let v=MagicVector3{array:Array([1.0,2.0,3.0])};
+	let dotty=v.dot(v);
+	println!("dot={dotty}");
+	println!("v.x={}",v.x);//wow!
 }
