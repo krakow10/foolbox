39bd91a2ef897b42d7d191fdf1c0d77b797462e4
Quaternions <krakow20@gmail.com>
2024-09-17 13:00:51 -0700

    cool cast

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-struct Five(u8);
-impl Default for Five{
-	fn default() -> Self {
-		Five(5)
-	}
-}
 fn main() {
-	//arrays only implement default up to length 32 !?
-	let arr:[Five;32]=Default::default();
-	let arr:[Five;33]=Default::default();
+	let is_negative=true;
+	let b=-(is_negative as i8);
+	println!("{b}");
 }
