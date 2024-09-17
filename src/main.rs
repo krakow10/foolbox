06509a13b80b88931ed2637ea97243a87a73e064
Quaternions <krakow20@gmail.com>
2024-09-16 19:22:48 -0700

    tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
 	let mut h=ahash::AHashSet::new();
 
-	for a in 1..=u16::MAX{
+	for a in 0..=(u16::MAX as u32){
 		for b in 1..=a{
-			let c=(a as u32)*(b as u32);
+			let c=a*b;
 			h.insert(c);
 		}
 	}
