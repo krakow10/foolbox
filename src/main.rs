93c2d654da2afd08ff755297d98662c3f1384c0b
Quaternions <krakow20@gmail.com>
2024-09-20 17:41:10 -0700

    ??????

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
+fn a_function<'lua,T,F:mlua::UserDataFields<'lua,T>+?Sized>(fields:&mut F){}
+
+pub trait Composition<'lua,F>:Sized
+	where
+		F:mlua::UserDataFields<'lua,Self>+'lua,
+{
+	const COMPOSITION:&'lua [fn(&mut F)];
+}
+
+struct Unit{
+	referent:u128,
+}
+
+struct Instance{
+	name:String,
+}
+impl Instance{}
+
+impl<'a,F> Composition<'a,F> for Instance
+where
+	F:mlua::UserDataFields<'a,Self>+'a{
+	const COMPOSITION:&'a [fn(&mut F)]=&[a_function];
+}
+
+impl mlua::UserData for T
+{
+	fn add_fields<'lua,F>(fields:&mut F)
+	where
+			F:mlua::UserDataFields<'lua,Self>+'lua,
+			T:Composition<'lua,F>,
+	{
+		T::COMPOSITION.iter().for_each(|f|f(fields))
+	}
+}
+
 fn main() {
-	let is_negative=true;
-	let b=-(is_negative as i8);
-	println!("{b}");
+	let mut a=DataModel::new();
+	a=Lighting::new();
 }
