511135a3a82468e64c3afa31f375b9f3120f625c
Quaternions <krakow20@gmail.com>
2024-12-23 17:27:08 -0800

    floaty float

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-use std::collections::HashMap;
-
 fn main() {
-	let mut h=HashMap::new();
-
-	let r=0..10;
-	for i in r{
-		h.entry(i%3)
-			.and_modify(|a|(*a)+=1)
-			.or_insert(0);
+	let not_0_01=0.01f32;
+	let mut s=0.0;
+	for _ in 0..100{
+		s+=not_0_01;
 	}
-
-	let all_mut:Vec<_>=h.iter_mut().collect();
-
-	println!("{:?}",h);
+	println!("{:.32}",s);
 }
