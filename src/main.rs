9dd43f1b582a7d4894a900fd1f5130c2e0b870c7
Quaternions <krakow20@gmail.com>
2025-01-13 21:41:49 -0800

    replace enum variant

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-fn main() {
-	let not_0_01=0.01f32;
-	let mut s=0.0;
-	for _ in 0..100{
-		s+=not_0_01;
+// Some type that does not implement the Copy trait
+struct NotCopy;
+
+// The enum which needs to change variants
+enum Propagate{
+	StageOne,
+	StageTwo(NotCopy),
+	StageThree(NotCopy),
+	StageFour,
+}
+
+// method to replace self with another variant
+impl Propagate{
+	fn propagate(&mut self){
+		*self=match *self{
+			Propagate::StageOne=>Propagate::StageTwo(NotCopy),
+			Propagate::StageTwo(not_copy)=>Propagate::StageThree(not_copy),
+			Propagate::StageThree(_not_copy)=>Propagate::StageFour,
+			Propagate::StageFour=>Propagate::StageOne,
+		}
 	}
-	println!("{:.32}",s);
+}
+
+fn main() {
+	let mut e=Propagate::StageOne;
+
+	e.propagate();
+	e.propagate();
+	e.propagate();
+	e.propagate();
 }
