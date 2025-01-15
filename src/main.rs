0204f2328cf068e4f233a97e60c86dd1e65e880b
Quaternions <krakow20@gmail.com>
2025-01-14 21:05:41 -0800

    stack list

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-fn main() {
-	let not_0_01=0.01f32;
-	let mut s=0.0;
-	for _ in 0..100{
-		s+=not_0_01;
+// A piece of data that does not implement the Copy trait
+#[derive(Debug)]
+struct NotCopy;
+
+enum StackListGenerator{
+	One,
+	Two,
+}
+
+fn run_stack_list(variant:StackListGenerator){
+	// How to write this code so that not_copy_owned is owned instead of a reference?
+	let stack_list:&[NotCopy]=match variant{
+		StackListGenerator::One=>&[NotCopy],
+		StackListGenerator::Two=>&[NotCopy,NotCopy],
+	};
+	for not_copy_owned in stack_list{
+		println!("List Value: {:?}",not_copy_owned);
 	}
-	println!("{:.32}",s);
+}
+
+fn main(){
+	run_stack_list(StackListGenerator::One);
+	run_stack_list(StackListGenerator::Two);
 }
