f1d4f98f981e1c3854b0e80d49d284866d780bc4
Quaternions <krakow20@gmail.com>
2025-01-18 10:27:09 -0800

    does hex have correct lower bits (0-9 yes a-f no)

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
-// A piece of data that does not implement the Copy trait
-#[derive(Debug)]
-struct NotCopy;
-
-enum StackListGenerator{
-	One,
-	Two,
-}
-
-fn run_stack_list(variant:StackListGenerator){
-	// How to write this code so that not_copy_owned is owned instead of a reference?
-	let stack_list:&[NotCopy]=match variant{
-		StackListGenerator::One=>&[NotCopy],
-		StackListGenerator::Two=>&[NotCopy,NotCopy],
-	};
-	for not_copy_owned in stack_list{
-		println!("List Value: {:?}",not_copy_owned);
-	}
-}
 
 fn main(){
-	run_stack_list(StackListGenerator::One);
-	run_stack_list(StackListGenerator::Two);
+	let a:Vec<u8>=(0..16).collect();
+	let b=b"0123456789abcdef";
+	for i in 0..16{
+		println!("{} lhs={:08b} rhs={:08b}",char::from(b[i]),a[i],b[i]);
+	}
 }
