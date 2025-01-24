b7106a8beaa433711516d57e7fa1c012cc12d801
Quaternions <krakow20@gmail.com>
2025-01-24 11:38:20 -0800

    make 255 images

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 fn main(){
 	let data=include_bytes!("../meshdata/385416572.meshdata");
-	let first=&data[0..23];
-	println!("first {first:?}");
-	let mut count=0;
-	let mut im=image::ImageBuffer::<image::Luma<u8>,_>::new(31,193);
-	for (y,chunk) in data[23..].chunks_exact(31).enumerate(){
-		count+=1;
-		for (x,b) in chunk.iter().enumerate(){
-			im.put_pixel(x as u32,y as u32,image::Luma([*b]));
-			print!("{b:3.},");
+	let offset=0;
+	for columns in 1..256{
+		let rows=data.len()/columns;
+		let first=&data[0..offset];
+		println!("first {first:?}");
+		let mut im=image::ImageBuffer::<image::Luma<u8>,_>::new(columns as u32,rows as u32);
+		for (y,chunk) in data[offset..].chunks_exact(columns).enumerate(){
+			for (x,b) in chunk.iter().enumerate(){
+				im.put_pixel(x as u32,y as u32,image::Luma([*b]));
+			}
 		}
-		println!();
+		let mut file=std::fs::File::create(format!("meshdata{columns}.png")).unwrap();
+		im.write_to(&mut file,image::ImageFormat::Png).unwrap();
 	}
-	println!("chunks={count}");
-	let mut file=std::fs::File::create("meshdata.png").unwrap();
-	im.write_to(&mut file,image::ImageFormat::Png).unwrap();
 }
