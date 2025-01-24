de1cab76253110c224097d1c405fa02fc460adb2
Quaternions <krakow20@gmail.com>
2025-01-24 12:10:33 -0800

    discover noise

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
+use std::collections::HashMap;
+
 fn main(){
 	let data=include_bytes!("../meshdata/385416572.meshdata");
-	let offset=0;
-	for columns in 1..256{
-		let rows=data.len()/columns;
-		let first=&data[0..offset];
-		println!("first {first:?}");
-		let mut im=image::ImageBuffer::<image::Luma<u8>,_>::new(columns as u32,rows as u32);
-		for (y,chunk) in data[offset..].chunks_exact(columns).enumerate(){
-			for (x,b) in chunk.iter().enumerate(){
-				im.put_pixel(x as u32,y as u32,image::Luma([*b]));
+	let columns=84;
+	let modulus=31;
+	let mut c=0;
+	let mut tally:[HashMap<u8,u32>;31]=Default::default();
+	for (y,chunk) in data.chunks_exact(columns).enumerate(){
+		for (x,&b) in chunk.iter().enumerate(){
+			c+=1;
+			if 34<=x&&x<=51&&1<y&&y<64{
+				let i=c%modulus;
+				*tally[i].entry(b).or_insert(0)+=1;
 			}
 		}
-		let mut file=std::fs::File::create(format!("meshdata{columns}.png")).unwrap();
-		im.write_to(&mut file,image::ImageFormat::Png).unwrap();
 	}
+	for (i,h) in tally.into_iter().enumerate(){
+		let mut k_max_v=0;
+		let mut max_v=0;
+		for (k,v) in h{
+			if max_v<v{
+				k_max_v=k;
+				max_v=v;
+			}
+		}
+		println!("noise[{i}]={k_max_v}");
+	}
+	let noise=[122,86,46,110,88,49,32,48,4,52,105,12,119,12,1,94,0,26,96,55,105,29,82,43,7,79,36,89,101,83,4];
 }
