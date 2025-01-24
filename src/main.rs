e07313f74a11d7fc2eb855066cedd27b803b90c5
Quaternions <krakow20@gmail.com>
2025-01-24 10:32:59 -0800

    data image

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ fn main(){
 	let data=include_bytes!("../385416572.meshdata");
 	let first=&data[0..23];
 	println!("first {first:?}");
-	let mut it=data[23..].chunks_exact(31);
 	let mut count=0;
-	for chunk in &mut it{
+	let mut im=image::ImageBuffer::<image::Luma<u8>,_>::new(31,193);
+	for (y,chunk) in data[23..].chunks_exact(31).enumerate(){
 		count+=1;
-		for b in chunk{
+		for (x,b) in chunk.iter().enumerate(){
+			im.put_pixel(x as u32,y as u32,image::Luma([*b]));
 			print!("{b:3.},");
 		}
 		println!();
 	}
 	println!("chunks={count}");
+	let mut file=std::fs::File::create("meshdata.png").unwrap();
+	im.write_to(&mut file,image::ImageFormat::Png).unwrap();
 }
