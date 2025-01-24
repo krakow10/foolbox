11ded8dd3d02aa5bd26e7e4e303b053b10947425
Quaternions <krakow20@gmail.com>
2025-01-24 10:17:25 -0800

    display meshdata

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-
 fn main(){
-	let a:Vec<u8>=(0..16).collect();
-	let b=b"0123456789abcdef";
-	for i in 0..16{
-		println!("{} lhs={:08b} rhs={:08b}",char::from(b[i]),a[i],b[i]);
+	let data=include_bytes!("../385416572.meshdata");
+	let first=&data[0..23];
+	println!("first {first:?}");
+	let mut it=data[23..].chunks_exact(31);
+	let mut count=0;
+	for chunk in &mut it{
+		count+=1;
+		for b in chunk{
+			print!("{b:3.},");
+		}
+		println!();
 	}
+	println!("chunks={count}");
 }
