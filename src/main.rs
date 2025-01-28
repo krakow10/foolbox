a83c36f4c4a2af75466b77a950390ed25d2acef6
Quaternions <krakow20@gmail.com>
2025-01-28 08:57:29 -0800

    try stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 fn main(){
-	let data=include_bytes!("../meshdata/394453730.meshdata");
-	let noise=[86,46,110,88,49,32,48,4,52,105,12,119,12,1,94,0,26,96,55,105,29,82,43,7,79,36,89,101,83,4,122];
-	let actual_data:Vec<_>=data.iter().enumerate().map(|(i,b)|b^noise[i%noise.len()]).collect();
-	std::fs::write("decoded_meshdata_394453730.meshdata",actual_data).unwrap();
+	let data=include_bytes!("../meshdata/15124417947_5.meshdata");
+	for columns in 4..84{
+		let rows=data.len()/columns;
+		let mut im=image::ImageBuffer::<image::Luma<u8>,_>::new(columns as u32,rows as u32);
+		for (y,chunk) in data.chunks_exact(columns).enumerate(){
+			for (x,b) in chunk.iter().enumerate(){
+				im.put_pixel(x as u32,y as u32,image::Luma([*b]));
+			}
+		}
+		let mut file=std::fs::File::create(format!("meshdata{columns}.png")).unwrap();
+		im.write_to(&mut file,image::ImageFormat::Png).unwrap();
+	}
 }
