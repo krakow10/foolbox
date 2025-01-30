68b4c67c7e5d4e753f25128bad1c7f29e630248a
Quaternions <krakow20@gmail.com>
2025-01-30 13:59:14 -0800

    test enum as usize

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-pub const OBFUSCATION_NOISE_CYCLE_XOR:[u8;31]=[86,46,110,88,49,32,48,4,52,105,12,119,12,1,94,0,26,96,55,105,29,82,43,7,79,36,89,101,83,4,122];
-fn reversible_obfuscate(offset:u64,buf:&mut [u8]){
-	const LEN:u64=OBFUSCATION_NOISE_CYCLE_XOR.len() as u64;
-	for (i,b) in buf.iter_mut().enumerate(){
-		*b^=OBFUSCATION_NOISE_CYCLE_XOR[((offset+i as u64)%LEN) as usize];
-	}
+pub enum NormalId2{
+	Right=1,
+	Top=2,
+	Back=3,
+	Left=4,
+	Bottom=5,
+	Front=6,
 }
 fn main(){
-	let data=b"CSGMDL\x04\0\0\0";
-	let mut datadata=*data;
-	reversible_obfuscate(0,&mut datadata);
-	println!("{datadata:?}");
+	let whoa=NormalId2::Right as usize;
+	println!("{whoa}");
 }
