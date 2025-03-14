82e7274c86076eaf3a3d3751e2760738f5ed2500
Quaternions <krakow20@gmail.com>
2025-03-13 18:03:37 -0700

    test masked signed int

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
-trait Z{
-    fn z(self);
-}
-// function x has generic type Y with trait bound Z
-fn x<Y:Z>(y:Y){
-    y.z()
-}
-
 fn main(){
-    struct Fella;
-    impl Z for Fella{
-        fn z(self){
-            println!("hello from Y using Z");
-        }
-    }
-    // allowed by trait bounds
-    x(Fella);
-
-    struct Goober;
-    // not allowed, Goober does not implement Z
-    // x(Goober); // compile error
+	const MASK:i32=!((1<<16)-1);
+	let numba=-1289828957;
+	dbg!(numba&MASK);
 }
