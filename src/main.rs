6e314ab3a78e5e25449dc22930ec1bd6dda7c918
Quaternions <krakow20@gmail.com>
2025-03-20 16:30:56 -0700

    square of points

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 fn main(){
-	const MASK:i32=!((1<<16)-1);
-	let numba=-1289828957;
-	dbg!(numba&MASK);
+	const GON:i32=3;
+	const POINTS:[[i32;2];24]=const{
+		let mut points=[[0;2];24];
+		let mut x=-GON;
+		while x<GON{
+			points[(x+GON) as usize]=[x,GON];
+			points[(x+GON+2*2*GON) as usize]=[-x,-GON];
+			x+=1;
+		}
+		let mut y=-GON;
+		while y<GON{
+			points[(y+GON+1*2*GON) as usize]=[GON,-y];
+			points[(y+GON+3*2*GON) as usize]=[-GON,y];
+			y+=1;
+		}
+		points
+	};
+	let mut s=format!("{:?}",POINTS);
+	s=s.replace('[',"{");
+	s=s.replace(']',"}");
+	println!("{s}");
 }
