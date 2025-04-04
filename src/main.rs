9eb08213074b859b2364ab587fa7e13f12b3125a
Quaternions <krakow20@gmail.com>
2025-04-04 14:28:47 -0700

    test signed masking

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
 fn main(){
-	const GON:i32=3;
-	const POINTS:[[i32;2];24]=const{
-		let mut points=[[0;2];24];
-		let mut x=-GON;
-		while x<GON{
-			points[(x+GON) as usize]=[x,GON];
-			points[(x+GON+2*2*GON) as usize]=[-x,-GON];
-			x+=1;
-		}
-		let mut y=-GON;
-		while y<GON{
-			points[(y+GON+1*2*GON) as usize]=[GON,-y];
-			points[(y+GON+3*2*GON) as usize]=[-GON,y];
-			y+=1;
-		}
-		points
-	};
-	let mut s=format!("{:?}",POINTS);
-	s=s.replace('[',"{");
-	s=s.replace(']',"}");
-	println!("{s}");
+	let all=-1i32;
+	let mask=1<<2;
+	println!("{}",all&mask);
 }
