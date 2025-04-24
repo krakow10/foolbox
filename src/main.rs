69e3e711d9ccd90a66d08ebad0b565c857b76bc6
Quaternions <krakow20@gmail.com>
2025-04-24 15:14:10 -0700

    write comments

--- src/main.rs
+++ src/main.rs
@@ -47,19 +47,26 @@ impl<'str> Words<'str>{
 fn main(){
 	let host=LifetimeHost;
 	let mut words=Words::new(&host);
-	// with a correct implementation, the following code should be valid:
+
+	// borrow Words mutably
 	let a=words.intern("bruh");
-	let b={
-		// create a scoped reference to words
-		let words=&words;
-		// get a word
-		words.get("bruh").unwrap()
-		// drop the scope reference
-	};
+	// drop mutable borrow and borrow immutably
+	let b=words.get("bruh").unwrap();
+	// compare both references; this is impossible when
+	// the lifetimes of a and b are derived from
+	// the borrows in .get and .intern
+	// e.g.
+	// fn    get<'a>(&'a     self,s:&str)->Option<&'a str>{
+	// fn intern<'a>(&'a mut self,s:&str)->       &'a str {
+	// instead of the lifetime of the underlying data 'str
 	println!("{}",a==b);
 
 	// with a correct implementation,
 	// dropping words here should introduce a compile error
 	drop(words);
+	println!("{}",a==b);
+
+	// dropping LifetimeHost gives the desired compile error
+	// drop(host);
 	println!("{}",a==b);
 }
