c3a6e00aac7a4645e240510374acdf2a545eef30
Quaternions <krakow20@gmail.com>
2025-05-02 18:42:20 -0700

    amazing test

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,4 @@
-use hash_str::hstr;
-use hash_str::{HashStr,HashStrMap};
-
-#[allow(dead_code)]
-#[derive(Debug)]
-enum Error{
-	Encode(rmp_serde::encode::Error),
-	Decode(rmp_serde::decode::Error),
-}
-
-#[derive(serde::Serialize,serde::Deserialize)]
-struct Outer<'a>{
-	#[serde(borrow)]
-	field:HashStrMap<'a,u32>,
-}
-
-fn main()->Result<(),Error>{
-	let hstr1=hstr!("bruh");
-	let hstr2=hstr!("yo");
-
-	let mut field=HashStrMap::default();
-
-	field.insert(hstr1, 1);
-	field.insert(hstr2, 2);
-
-	let outer=Outer{
-		field,
-	};
-
-	let j=rmp_serde::encode::to_vec(&outer).map_err(Error::Encode)?;
-
-	let outer2:Outer=rmp_serde::decode::from_slice(&j).map_err(Error::Decode)?;
-
-	println!("hstr1={:?}",outer2.field.get(hstr1));
-	println!("hstr2={:?}",outer2.field.get(hstr2));
-
-	Ok(())
+fn main(){
+	let bytes=include_bytes!("/run/media/quat/Files/Documents/map-files/verify-scripts/maps/surf_all/surf_clique2.rbxm");
+	rbx_binary::from_reader(bytes.as_slice()).unwrap();
 }
