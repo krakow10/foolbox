379d33a3951e4851aea08c1964b333600c0c9bfa
Quaternions <krakow20@gmail.com>
2025-05-09 18:25:08 -0700

    goodbye

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,2 @@
 fn main(){
-	let bytes=include_bytes!("/run/media/quat/Files/Documents/map-files/verify-scripts/maps/surf_all/surf_clique2.rbxm");
-	rbx_binary::from_reader(bytes.as_slice()).unwrap();
 }
