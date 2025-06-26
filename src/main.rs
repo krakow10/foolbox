bc8485fd52880c012b3dd77d702eb6e4d0191f5f
Quaternions <krakow20@gmail.com>
2025-06-26 00:14:55 -0700

    test nova driver panic

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
-use std::collections::HashSet;
-use rbx_dom_weak::{types::Variant,ustr};
 
 fn main(){
-	let db=rbx_reflection_database::get();
-	let base_script=&db.classes["LuaSourceContainer"];
-	let file=std::fs::read(std::env::args().skip(1).next().unwrap()).unwrap();
-	let dom=rbx_binary::from_reader(file.as_slice()).unwrap();
+	let buf=&[0,1,2,3,4];
 
-	let mut unique_scripts=HashSet::new();
-	for instance in dom.descendants(){
-		let class=db.classes.get(instance.class.as_str()).unwrap();
-		if db.has_superclass(class,base_script){
-			if let Some(Variant::String(source))=instance.properties.get(&ustr("Source")){
-				unique_scripts.insert(source.as_str());
-			}
-		}
+	if buf.len()<5{
+		panic!();
 	}
-	println!("unique scripts: {}",unique_scripts.len());
+
+	let slice=&buf[2..6];
+	println!("{slice:?}");
 }
