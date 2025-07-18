8f4c9c7d13b957207920a8271033e1db7ada7f9f
Rhys Lloyd <krakow20@gmail.com>
2025-07-18 00:17:28 -0700

    set struct size

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+use std::mem::offset_of;
 
+#[derive(Debug)]
+struct EINVAL;
+
+#[derive(Debug)]
+struct PmuLookupTableHeader {
+    version: u8,
+    header_len: u8,
+    entry_len: u8,
+    entry_count: u8,
+}
+
+impl PmuLookupTableHeader {
+    // TODO[TRSM]: use FromBytes::from_bytes when it becomes available.
+    fn new(data: &[u8]) -> Result<Self,EINVAL> {
+        if data.len() < core::mem::size_of::<Self>() {
+            return Err(EINVAL);
+        }
+
+        Ok(PmuLookupTableHeader {
+            version: data[const{offset_of!(PmuLookupTableHeader,version)}],
+            header_len: data[const{offset_of!(PmuLookupTableHeader,header_len)}],
+            entry_len: data[const{offset_of!(PmuLookupTableHeader,entry_len)}],
+            entry_count: data[const{offset_of!(PmuLookupTableHeader,entry_count)}],
+        })
+    }
+}
 fn main(){
-	let buf=&[0,1,2,3,4];
-
-	if buf.len()<5{
-		panic!();
-	}
-
-	let slice=&buf[2..6];
-	println!("{slice:?}");
+	let a=PmuLookupTableHeader::new(&[1,2,3,4]);
+	dbg!(a);
 }
