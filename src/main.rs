91b923496d087587dd1a261ecbce937d7d4f9817
Rhys Lloyd <krakow20@gmail.com>
2025-07-26 01:25:41 -0700

    test BitHeader

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-use std::mem::offset_of;
-
-#[derive(Debug)]
-struct EINVAL;
-
-#[derive(Debug)]
-struct PmuLookupTableHeader {
-    version: u8,
-    header_len: u8,
-    entry_len: u8,
-    entry_count: u8,
+#[repr(C)]
+struct BitHeader {
+    /// 0h: BIT Header Identifier (BMP=0x7FFF/BIT=0xB8FF)
+    id: u16,
+    /// 2h: BIT Header Signature ("BIT\0")
+    signature: [u8; 4],
+    /// 6h: Binary Coded Decimal Version, ex: 0x0100 is 1.00.
+    bcd_version: u16,
+    /// 8h: Size of BIT Header (in bytes)
+    header_size: u8,
+    /// 9h: Size of BIT Tokens (in bytes)
+    token_size: u8,
+    /// 10h: Number of token entries that follow
+    token_entries: u8,
+    /// 11h: BIT Header Checksum
+    checksum: u8,
 }
-
-impl PmuLookupTableHeader {
-    // TODO[TRSM]: use FromBytes::from_bytes when it becomes available.
-    fn new(data: &[u8]) -> Result<Self,EINVAL> {
-        if data.len() < core::mem::size_of::<Self>() {
-            return Err(EINVAL);
-        }
-
-        Ok(PmuLookupTableHeader {
-            version: data[const{offset_of!(PmuLookupTableHeader,version)}],
-            header_len: data[const{offset_of!(PmuLookupTableHeader,header_len)}],
-            entry_len: data[const{offset_of!(PmuLookupTableHeader,entry_len)}],
-            entry_count: data[const{offset_of!(PmuLookupTableHeader,entry_count)}],
-        })
-    }
-}
-fn main(){
-	let a=PmuLookupTableHeader::new(&[1,2,3,4]);
-	dbg!(a);
+fn main() {
+    let a = PmuLookupTableHeader::new(&[1, 2, 3, 4]);
+    dbg!(a);
 }
