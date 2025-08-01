c5393b685b28e5336a43e963e04824b0ce70588a
Rhys Lloyd <krakow20@gmail.com>
2025-08-01 06:00:19 -0700

    try again

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ fn main() {
 	// the value, a "static" meaning it
 	// exists for the entire lifetime of the program, not just this function call,
 	// and always refers to one singular global value.
-	static mut VALUE:i64=0;
+	static mut VALUE:&mut i64=&mut 0;
 
 	// how many cpu cores do we have to work with?
 	let parallelism = std::thread::available_parallelism().unwrap().get();
+
+	println!("threads: {parallelism}");
 
 	// spawn threads attempting to add one
 	for _ in 0..parallelism/2{
 		std::thread::spawn(||{
 			loop{
 				// this is undefined behaviour!
-				unsafe{(*&mut VALUE)+=1}
+				unsafe{*VALUE+=1}
 			}
 		});
 	}
@@ -21,14 +23,14 @@ fn main() {
 		std::thread::spawn(||{
 			loop{
 				// this is undefined behaviour!
-				unsafe{(*&mut VALUE)-=1}
+				unsafe{*VALUE-=1}
 			}
 		});
 	}
 
 	// monitor the chaos every 100ms
 	loop{
-		println!("{}",unsafe{VALUE});
+		println!("{}",unsafe{*VALUE});
 		std::thread::sleep(std::time::Duration::from_millis(100));
 	}
 }
