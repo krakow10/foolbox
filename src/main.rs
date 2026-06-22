d3ec9956f3c8bddeeae7141bf46910bf39e989dd
Rhys Lloyd <krakow20@gmail.com>
2026-06-22 12:24:55 -0700

    the

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
-fn main() {
-	// the value, a "static" meaning it
-	// exists for the entire lifetime of the program, not just this function call,
-	// and always refers to one singular global value.
-	static mut VALUE:&mut i64=&mut 0;
+use std::collections::HashMap;
 
-	// how many cpu cores do we have to work with?
-	let parallelism = std::thread::available_parallelism().unwrap().get();
-
-	println!("threads: {parallelism}");
-
-	// spawn threads attempting to add one
-	for _ in 0..parallelism/2{
-		std::thread::spawn(||{
-			loop{
-				// this is undefined behaviour!
-				unsafe{*VALUE+=1}
-			}
-		});
-	}
-	// spawn threads attempting to subtract one
-	for _ in 0..parallelism/2{
-		std::thread::spawn(||{
-			loop{
-				// this is undefined behaviour!
-				unsafe{*VALUE-=1}
-			}
-		});
+fn main(){
+	struct Battle{
+		timeout:f32,
+		player:&'static str,
 	}
 
-	// monitor the chaos every 100ms
-	loop{
-		println!("{}",unsafe{*VALUE});
-		std::thread::sleep(std::time::Duration::from_millis(100));
+	let mut battles=HashMap::new();
+	battles.insert("BlubBlub",
+		Battle{
+			timeout:5.0,
+			player:"Quaternions",
+		}
+	);
+
+	let time_now=6.0;
+
+	for (fish,battle) in battles.iter_mut(){
+		if battle.timeout<time_now{
+			battles.remove(fish);
+		}
 	}
 }
