2fd3c2c2029ec5edcdab6ed0a0529790f0de227f
Rhys Lloyd <krakow20@gmail.com>
2026-07-10 21:58:07 -0700

    words

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
-use std::collections::HashMap;
-
 fn main(){
-	struct Battle{
-		timeout:f32,
-		player:&'static str,
-	}
-
-	let mut battles=HashMap::new();
-	battles.insert("BlubBlub",
-		Battle{
-			timeout:5.0,
-			player:"Quaternions",
-		}
-	);
-
-	let time_now=6.0;
-
-	for (fish,battle) in battles.iter_mut(){
-		if battle.timeout<time_now{
-			battles.remove(fish);
-		}
+	let words=std::fs::read_to_string("/usr/share/dict/words").unwrap();
+	for word in words.split_whitespace().filter(|word|word.contains("meow")){
+		println!("{wabber}",wabber=word);
 	}
 }
